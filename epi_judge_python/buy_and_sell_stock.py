2caa7eefb2cf8fce7c62f50b87c034cdd7056649
Nile Grice <nomen@nesci.us>
2022-11-17 17:32:20 -0700

    5.6 - Buy and sell a stock once

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,9 +4,18 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
-
+    low, high = float('inf'), 0
+    trade = 0
+    for price in prices:
+        if price < low:
+            trade = max(trade, high-low)
+            low = price
+            high = 0
+        elif price > high:
+            high = price
+    trade = max(trade, high-low)            
+    return trade
+            
 
 if __name__ == '__main__':
     exit(
