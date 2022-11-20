cd1e319207e89867e8efa178c13c90233233937d
Nile Grice <nomen@nesci.us>
2022-11-20 06:43:20 -0700

    6.1 - String <-> Int

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,16 +1,38 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+import string
 
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    res = []
+    negative = False
+    if x < 0:
+        negative, x = True, -x
+
+    while x:
+        digit = x % 10
+        res.append(chr(ord('0')+digit))
+        x //= 10
+
+    res.append('0')
+    res = ('-' if negative else '') + ''.join(reversed(res))
+
+    return res
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    res = 0
+    negative = False
+    if s[0] == '-':
+        negative = True
+        
+    for c in s:
+        if c in '-+':
+            continue
+        res *= 10
+        res += string.digits.index(c)
 
+    return -res if negative else res
 
 def wrapper(x, s):
     if int(int_to_string(x)) != x:
